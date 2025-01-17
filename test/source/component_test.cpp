8b5a8d9f7eae2e2c01ca608220f0046b0fbe9806
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2025-01-17 07:08:16 -0500

    Add component tests

---
+++ test/source/component_test.cpp
@@ -0,0 +1,18 @@
+#include <string>
+
+#include "yasf/component.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("component: name", "[library]")
+{
+    auto const exported = yasf::component{};
+    REQUIRE(exported.name() == std::string("component"));
+}
+
+TEST_CASE("component: set_name", "[library]")
+{
+    auto obj = yasf::component{};
+    obj.set_name("yasf");
+    REQUIRE(obj.name() == std::string{"yasf"});
+}
