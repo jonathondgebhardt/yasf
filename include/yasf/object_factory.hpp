1f1cdcdbb39378f4885f0eefcffc1396456135a1
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2025-01-17 11:11:29 -0500

    Add object_factory class

---
+++ include/yasf/object_factory.hpp
@@ -0,0 +1,26 @@
+
+#pragma once
+
+#include <memory>
+
+#include "yasf/object.hpp"
+#include "yasf/yasf_export.hpp"
+
+namespace yasf
+{
+
+/**
+ * @brief Reports the name of the library
+ */
+class YASF_EXPORT object_factory
+{
+public:
+    static auto build() -> std::unique_ptr<object>;
+
+private:
+    // TODO: hopefully i can read an object's definition from a file or
+    // something
+    auto build_impl() -> std::unique_ptr<object>;
+};
+
+}  // namespace yasf
---
+++ source/object_factory.cpp
@@ -0,0 +1,27 @@
+#include <memory>
+
+#include "yasf/object.hpp"
+
+#include "yasf/object_factory.hpp"
+#include "yasf/status.hpp"
+#include "yasf/uuid.hpp"
+
+namespace yasf
+{
+
+auto object_factory::build() -> std::unique_ptr<object>
+{
+    auto builder = object_factory{};
+    return builder.build_impl();
+}
+
+auto object_factory::build_impl() -> std::unique_ptr<object>
+{
+    // TODO: add components/children
+    auto obj = std::make_unique<object>();
+    obj->add_component(std::make_unique<status>());
+    obj->add_component(std::make_unique<uuid>());
+    return obj;
+}
+
+}  // namespace yasf
