b70776666eb0816d17b39d7476ce27fcf7a465d1
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2025-01-17 07:08:07 -0500

    Add component class

---
+++ include/yasf/component.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <string>
+#include <string_view>
+
+#include "yasf/yasf_export.hpp"
+
+namespace yasf
+{
+
+/**
+ * @brief Reports the name of the library
+ */
+class YASF_EXPORT component
+{
+public:
+    /**
+     * @brief Initializes the name field to the name of the project
+     */
+    component();
+
+    /**
+     * @brief Returns a non-owning pointer to the string stored in this class
+     */
+    auto name() const -> std::string_view;
+
+    auto set_name(std::string name) -> void;
+
+private:
+    YASF_SUPPRESS_C4251
+    std::string m_name;
+};
+
+}  // namespace yasf
---
+++ source/component.cpp
@@ -0,0 +1,24 @@
+#include <string_view>
+#include <utility>
+
+#include "yasf/component.hpp"
+
+namespace yasf
+{
+
+component::component()
+    : m_name{"component"}
+{
+}
+
+auto component::name() const -> std::string_view
+{
+    return m_name;
+}
+
+auto component::set_name(std::string name) -> void
+{
+    m_name = std::move(name);
+}
+
+}  // namespace yasf
