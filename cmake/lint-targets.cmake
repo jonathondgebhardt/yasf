524a2581210fa985ed396cf929c3ee9ceb7d81c8
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2025-01-18 15:50:46 -0500

    Revert "Attempt to skip format check"
    This reverts commit aa7a3b035211cae4c84022e63d687b9a6b5e1d4d.

--- cmake/lint-targets.cmake
+++ cmake/lint-targets.cmake
@@ -9,16 +9,16 @@ set(
 
 set(FORMAT_COMMAND clang-format CACHE STRING "Formatter to use")
 
-# add_custom_target(
-#     format-check
-#     COMMAND "${CMAKE_COMMAND}"
-#     -D "FORMAT_COMMAND=${FORMAT_COMMAND}"
-#     -D "PATTERNS=${FORMAT_PATTERNS}"
-#     -P "${PROJECT_SOURCE_DIR}/cmake/lint.cmake"
-#     WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
-#     COMMENT "Linting the code"
-#     VERBATIM
-# )
+add_custom_target(
+    format-check
+    COMMAND "${CMAKE_COMMAND}"
+    -D "FORMAT_COMMAND=${FORMAT_COMMAND}"
+    -D "PATTERNS=${FORMAT_PATTERNS}"
+    -P "${PROJECT_SOURCE_DIR}/cmake/lint.cmake"
+    WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
+    COMMENT "Linting the code"
+    VERBATIM
+)
 
 add_custom_target(
     format-fix
