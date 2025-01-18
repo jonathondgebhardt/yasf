0af45907484d2552d9070cee61ed830bfc3f566f
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2025-01-17 22:35:31 -0500

    Add position class

---
+++ include/yasf/position.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "yasf/component.hpp"
+#include "yasf/vec3d.hpp"
+#include "yasf/yasf_export.hpp"
+
+namespace yasf
+{
+
+/**
+ * @brief Reports the name of the library
+ */
+class YASF_EXPORT position : public component
+{
+public:
+    /**
+     * @brief Initializes the name field to the name of the project
+     */
+    position()
+        : component("position")
+    {
+    }
+
+    auto get() const -> vec3d { return m_vec; }
+
+    auto set(vec3d vec) -> void { m_vec = vec; }
+
+private:
+    YASF_SUPPRESS_C4251
+    vec3d m_vec;
+};
+
+}  // namespace yasf
