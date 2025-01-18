26682d7a868ebe207eb44867e207cb14c5f9a0fa
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2025-01-18 16:49:15 -0500

    Add time_updater interface

---
+++ include/yasf/time_updater.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "yasf/component.hpp"
+#include "yasf/types.hpp"
+#include "yasf/yasf_export.hpp"
+
+namespace yasf
+{
+
+class time_updater : public component
+{
+public:
+    virtual auto next_time(time_usec current_time, time_sec delta)
+        -> time_usec = 0;
+
+protected:
+    explicit time_updater(std::string name)
+        : component(std::move(name))
+    {
+    }
+
+private:
+    YASF_SUPPRESS_C4251
+};
+
+}  // namespace yasf
