16878303d7ced5023d047c2cf6c8bfed16865cca
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2025-01-18 08:34:45 -0500

    Test that parent is set in add_component

--- test/source/component_test.cpp
+++ test/source/component_test.cpp
@@ -1,14 +1,38 @@
+#include <memory>
 #include <string>
 
 #include "yasf/component.hpp"
 
 #include <catch2/catch_test_macros.hpp>
 
+#include "yasf/object.hpp"
+
 TEST_CASE("component: name", "[library]")
 {
     SECTION("default")
     {
-        auto const exported = yasf::component{};
-        REQUIRE(exported.name() == std::string("component"));
+        auto const component = yasf::component{};
+        REQUIRE(component.name() == std::string("component"));
+    }
+}
+
+TEST_CASE("component: parent", "[library]")
+{
+    auto obj = yasf::object{};
+
+    SECTION("take by value")
+    {
+        REQUIRE(obj.add_component(std::make_unique<yasf::component>()));
+        auto const* component = obj.get_component("component");
+        REQUIRE(component != nullptr);
+        REQUIRE(component->parent() != nullptr);
+    }
+
+    SECTION("templated")
+    {
+        REQUIRE(obj.add_component<yasf::component>());
+        auto const* component = obj.get_component("component");
+        REQUIRE(component != nullptr);
+        REQUIRE(component->parent() != nullptr);
     }
 }
