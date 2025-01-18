041193259f243dd33dbfd1cb5bbbd4ba773704be
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2025-01-18 17:06:33 -0500

    Use clock_factory

--- test/source/clock_test.cpp
+++ test/source/clock_test.cpp
@@ -2,8 +2,8 @@
 
 #include <catch2/catch_test_macros.hpp>
 
+#include "yasf/clock_factory.hpp"
 #include "yasf/convert.hpp"
-#include "yasf/fixed_time_updater.hpp"
 #include "yasf/math.hpp"
 #include "yasf/types.hpp"
 
@@ -39,31 +39,28 @@ TEST_CASE("tick: no updater", "[clock]")
 
 TEST_CASE("tick: one second delta", "[clock]")
 {
-    constexpr auto one_second_delta = yasf::time_sec{1.0};
-    auto clock = yasf::clock{one_second_delta};
-    REQUIRE(clock.add_component<yasf::fixed_time_updater>());
+    auto clock = yasf::clock_factory::build_fixed_update(1.0);
 
     constexpr auto iterations = 10;
     for (auto i = 0; i < iterations; ++i) {
-        clock.tick();
-        REQUIRE(clock.time() == yasf::convert::sec_to_usec(i + 1));
+        clock->tick();
+        REQUIRE(clock->time() == yasf::convert::sec_to_usec(i + 1));
     }
 }
 
 TEST_CASE("tick: change delta", "[clock]")
 {
     constexpr auto one_second_delta = yasf::time_sec{1.0};
-    auto clock = yasf::clock{one_second_delta};
-    REQUIRE(clock.add_component<yasf::fixed_time_updater>());
+    auto clock = yasf::clock_factory::build_fixed_update(one_second_delta);
 
-    clock.tick();
-    REQUIRE(clock.time() == yasf::convert::sec_to_usec(one_second_delta));
+    clock->tick();
+    REQUIRE(clock->time() == yasf::convert::sec_to_usec(one_second_delta));
 
     constexpr auto two_second_delta = yasf::time_sec{2.0};
-    clock.set_delta(two_second_delta);
-    auto const offset = clock.time();
+    clock->set_delta(two_second_delta);
+    auto const offset = clock->time();
 
-    clock.tick();
-    REQUIRE(clock.time()
+    clock->tick();
+    REQUIRE(clock->time()
             == offset + yasf::convert::sec_to_usec(two_second_delta));
 }
