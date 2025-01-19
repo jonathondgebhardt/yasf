59235e72716eeab49fc6ab9ce2c14facc65f5116
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2025-01-19 12:10:35 -0500

    Use visitor to move entities

--- source/mover.cpp
+++ source/mover.cpp
@@ -5,41 +5,59 @@
 #include "yasf/entity.hpp"
 #include "yasf/position.hpp"
 #include "yasf/velocity.hpp"
+#include "yasf/visitor.hpp"
 
 namespace yasf
 {
 
+struct mover_visitor : public object_visitor
+{
+    void visit(object* obj) override
+    {
+        auto* ent = dynamic_cast<entity*>(obj);
+        if (ent != nullptr) {
+            move_entity(ent);
+        }
+    }
+
+    auto move_entity(entity* entity) const -> void
+    {
+        yasf::ensure(m_clock != nullptr, "failed to access clock");
+
+        auto* const vel = entity->get_component<yasf::velocity>();
+        if (vel == nullptr || vel->get().is_zero()) {
+            return;
+        }
+
+        auto* pos = entity->get_component<yasf::position>();
+        if (pos == nullptr) {
+            return;
+        }
+
+        auto const delta_time = m_clock->delta_sec();
+
+        auto pos_vec = pos->get();
+        auto const vel_vec = vel->get();
+        pos_vec += vel_vec * delta_time;
+        pos->set(pos_vec);
+    }
+
+    clock* m_clock{};
+};
+
 auto mover::update() -> void
 {
-    auto* const service = get_entity_service();
-    ensure(service != nullptr, "failed to get entity_service");
+    auto* const esvc = get_entity_service();
+    ensure(esvc != nullptr, "failed to get entity_service");
+
+    auto* const clock = get_clock();
+    auto visitor = mover_visitor{};
+
+    // TODO: used designated initializer
+    visitor.m_clock = clock;
 
     // TODO: how do i know which entities to get? should they be tagged somehow?
-    // also, how do i get them? visitor pattern?
-    move_entity(service->get_child<entity>());
-}
-
-auto mover::move_entity(entity* entity) -> void
-{
-    auto* const clock = get_clock();
-    yasf::ensure(clock != nullptr, "failed to access clock");
-
-    auto* const vel = entity->get_component<yasf::velocity>();
-    if (vel == nullptr || vel->get().is_zero()) {
-        return;
-    }
-
-    auto* pos = entity->get_component<yasf::position>();
-    if (pos == nullptr) {
-        return;
-    }
-
-    auto const delta_time = clock->delta_sec();
-
-    auto pos_vec = pos->get();
-    auto const vel_vec = vel->get();
-    pos_vec += vel_vec * delta_time;
-    pos->set(pos_vec);
+    esvc->accept(visitor);
 }
 
 }  // namespace yasf
