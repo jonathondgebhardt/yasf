12fd685b4b563cc1d1140632406e46e57b5a394f
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2025-01-19 07:14:24 -0500

    Update test for framework changes

--- test/source/mover_test.cpp
+++ test/source/mover_test.cpp
@@ -1,95 +1,111 @@
+#include <memory>
+
 #include "yasf/mover.hpp"
 
 #include <catch2/catch_test_macros.hpp>
 
 #include "yasf/clock_factory.hpp"
 #include "yasf/entity_factory.hpp"
+#include "yasf/entity_service.hpp"
 #include "yasf/math.hpp"
 #include "yasf/position.hpp"
+#include "yasf/processor_service.hpp"
+#include "yasf/simulation.hpp"
 #include "yasf/velocity.hpp"
 
-// NOLINTBEGIN(readability-function-cognitive-complexity)
-TEST_CASE("mover: process")
+struct mover_fixture
 {
-    auto entity = yasf::entity_factory::build();
+    mover_fixture()
+    {
+        auto clock = yasf::clock_factory::build_fixed_update(m_delta_time);
+        m_sim = std::make_unique<yasf::simulation>(std::move(clock));
 
-    auto* pos = entity->get_component<yasf::position>();
-    REQUIRE(pos != nullptr);
-    REQUIRE(pos->get().is_zero());
+        m_sim->add_child<yasf::entity_service>();
+        auto* esvc = m_sim->get_child<yasf::entity_service>();
 
-    auto* vel = entity->get_component<yasf::velocity>();
+        esvc->add_child(yasf::entity_factory::build());
+        m_entity = esvc->get_child<yasf::entity>();
+
+        m_sim->add_child<yasf::processor_service>();
+        auto* psvc = m_sim->get_child<yasf::processor_service>();
+
+        psvc->add_child<yasf::mover>();
+        m_mover = psvc->get_child<yasf::mover>();
+    }
+
+    std::unique_ptr<yasf::simulation> m_sim;
+    yasf::mover* m_mover{};
+    yasf::entity* m_entity{};
+    static constexpr auto m_delta_time{1.0};
+};
+
+TEST_CASE_METHOD(mover_fixture, "mover: 1-d movement", "[processor]")
+{
+    constexpr auto x_vel = 1.0;
+    auto* vel = m_entity->get_component<yasf::velocity>();
     REQUIRE(vel != nullptr);
 
-    auto mover = yasf::mover{};
-    mover.set_root(entity.get());
+    vel->set(yasf::vec3d{x_vel, 0.0, 0.0});
 
-    auto clock = yasf::clock_factory::build_fixed_update(1.0);
+    auto* pos = m_entity->get_component<yasf::position>();
+    REQUIRE(pos != nullptr);
 
-    SECTION("throws without clock")
-    {
-        CHECK_THROWS(mover.process());
+    constexpr auto iterations = 10;
+    for (auto i = 1; i <= iterations; ++i) {
+        m_sim->get_clock()->tick();
+        m_mover->process();
+
+        auto const pos_vec = pos->get();
+        CHECK(yasf::math::double_eq(pos_vec.x(), x_vel * i));
     }
+}
 
-    mover.set_clock(clock.get());
+TEST_CASE_METHOD(mover_fixture, "mover: 2-d movement", "[processor]")
+{
+    constexpr auto x_vel = 1.0;
+    constexpr auto y_vel = 2.0;
+    auto* vel = m_entity->get_component<yasf::velocity>();
+    REQUIRE(vel != nullptr);
 
-    SECTION("no movement")
-    {
-        REQUIRE(vel->get().is_zero());
+    vel->set(yasf::vec3d{x_vel, y_vel, 0.0});
 
-        auto const initial_pos = pos->get();
-        mover.process();
-        REQUIRE(pos->get() == initial_pos);
+    auto* pos = m_entity->get_component<yasf::position>();
+    REQUIRE(pos != nullptr);
+
+    constexpr auto iterations = 10;
+    for (auto i = 1; i <= iterations; ++i) {
+        m_sim->get_clock()->tick();
+        m_mover->process();
+
+        auto const pos_vec = pos->get();
+        CHECK(yasf::math::double_eq(pos_vec.x(), x_vel * i));
+        CHECK(yasf::math::double_eq(pos_vec.y(), y_vel * i));
     }
+}
 
-    SECTION("1-d movement")
-    {
-        constexpr auto x_vel = 1.0;
-        vel->set(yasf::vec3d{x_vel, 0.0, 0.0});
+// NOLINTBEGIN(readability-function-cognitive-complexity)
+TEST_CASE_METHOD(mover_fixture, "mover: 3-d movement", "[processor]")
+{
+    constexpr auto x_vel = 1.0;
+    constexpr auto y_vel = 2.0;
+    constexpr auto z_vel = 3.0;
+    auto* vel = m_entity->get_component<yasf::velocity>();
+    REQUIRE(vel != nullptr);
 
-        constexpr auto iterations = 10;
-        for (auto i = 1; i <= iterations; ++i) {
-            clock->tick();
+    vel->set(yasf::vec3d{x_vel, y_vel, z_vel});
 
-            mover.process();
-            auto const pos_vec = pos->get();
-            REQUIRE(yasf::math::double_eq(pos_vec.x(), x_vel * i));
-        }
-    }
+    auto* pos = m_entity->get_component<yasf::position>();
+    REQUIRE(pos != nullptr);
 
-    SECTION("2-d movement")
-    {
-        constexpr auto x_vel = 1.0;
-        constexpr auto y_vel = 2.0;
-        vel->set(yasf::vec3d{x_vel, y_vel, 0.0});
+    constexpr auto iterations = 10;
+    for (auto i = 1; i <= iterations; ++i) {
+        m_sim->get_clock()->tick();
+        m_mover->process();
 
-        constexpr auto iterations = 10;
-        for (auto i = 1; i <= iterations; ++i) {
-            clock->tick();
-
-            mover.process();
-            auto const pos_vec = pos->get();
-            REQUIRE(yasf::math::double_eq(pos_vec.x(), x_vel * i));
-            REQUIRE(yasf::math::double_eq(pos_vec.y(), y_vel * i));
-        }
-    }
-
-    SECTION("3-d movement")
-    {
-        constexpr auto x_vel = 1.0;
-        constexpr auto y_vel = 2.0;
-        constexpr auto z_vel = 3.0;
-        vel->set(yasf::vec3d{x_vel, y_vel, z_vel});
-
-        constexpr auto iterations = 10;
-        for (auto i = 1; i <= iterations; ++i) {
-            clock->tick();
-
-            mover.process();
-            auto const pos_vec = pos->get();
-            REQUIRE(yasf::math::double_eq(pos_vec.x(), x_vel * i));
-            REQUIRE(yasf::math::double_eq(pos_vec.y(), y_vel * i));
-            REQUIRE(yasf::math::double_eq(pos_vec.z(), z_vel * i));
-        }
+        auto const pos_vec = pos->get();
+        CHECK(yasf::math::double_eq(pos_vec.x(), x_vel * i));
+        CHECK(yasf::math::double_eq(pos_vec.y(), y_vel * i));
+        CHECK(yasf::math::double_eq(pos_vec.z(), z_vel * i));
     }
 }
 
