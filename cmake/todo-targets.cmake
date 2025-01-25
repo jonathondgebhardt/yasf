516a2b4bb45ba51866e59e9c143f0a913e9d4bd8
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2025-01-25 08:53:20 -0500

    Cache list of bad words

--- cmake/todo-targets.cmake
+++ cmake/todo-targets.cmake
@@ -6,12 +6,12 @@ set( FORMAT_PATTERNS
 	"; separated patterns relative to the project source dir to grep"
 )
 
-set(TODO_COMMAND grep CACHE STRING "Finder to use")
+set(BAD_WORDS "TODO;FIXME" CACHE STRING "Bad words to check")
 
 add_custom_target(
 	todo-check
 	COMMAND "${CMAKE_COMMAND}"
-	-D "FORMAT_COMMAND=${FORMAT_COMMAND}"
+	-D "BAD_WORDS=${BAD_WORDS}"
 	-D "PATTERNS=${FORMAT_PATTERNS}"
 	-P "${PROJECT_SOURCE_DIR}/cmake/todo.cmake"
 	WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
