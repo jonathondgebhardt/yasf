2196669ee105b2a90da7603dc58d1c2db2068bd5
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2025-01-25 08:10:27 -0500

    Show line number if file fails TODO check

--- cmake/todo.cmake
+++ cmake/todo.cmake
@@ -13,6 +13,8 @@ default(PATTERNS
 	test/*.cpp test/*.hpp
 )
 
+set(flags "-n")
+
 file(GLOB_RECURSE files ${PATTERNS})
 set(contains_todo "")
 set(output "")
@@ -20,15 +22,14 @@ string(LENGTH "${CMAKE_SOURCE_DIR}/" path_prefix_length)
 
 foreach(file IN LISTS files)
 	execute_process(
-		COMMAND "${TODO_COMMAND}" "TODO" "${file}"
+		COMMAND "${TODO_COMMAND}" "${flags}" "TODO" "${file}"
 		WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
 		RESULT_VARIABLE result
 		OUTPUT_VARIABLE output
 	)
-	if(NOT result EQUAL "0")
-		message(WARNING "'${file}': finder returned with ${result}")
-	endif()
-	if(output MATCHES "TODO")
+	if(result EQUAL "0")
+		message(WARNING "'${file}': finder returned with ${result}\n${output}")
+
 		string(SUBSTRING "${file}" "${path_prefix_length}" -1 relative_file)
 		list(APPEND contains_todo "${relative_file}")
 	endif()
