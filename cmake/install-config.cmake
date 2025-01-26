c78b988f2dfd968e795037bbcf764f4eb26b9079
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2025-01-26 07:26:04 -0500

    Fix how boost uuid is found when installed

--- cmake/install-config.cmake
+++ cmake/install-config.cmake
@@ -1,7 +1,7 @@
 set(yasf_FOUND YES)
 
 include(CMakeFindDependencyMacro)
-find_dependency(Boost::uuid)
+find_dependency(boost_uuid)
 
 if(yasf_FOUND)
   include("${CMAKE_CURRENT_LIST_DIR}/yasfTargets.cmake")
