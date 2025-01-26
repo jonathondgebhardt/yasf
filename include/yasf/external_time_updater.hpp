700115e4be605d1032ba40ea02d9e18d71397cfc
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2025-01-26 07:53:29 -0500

    Add external_time_updater class

---
+++ include/yasf/external_time_updater.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "yasf/time_updater.hpp"
+#include "yasf/types.hpp"
+#include "yasf/yasf_export.hpp"
+
+namespace yasf
+{
+
+// TODO: should yasf have an abstraction for pumping events?
+// maybe that's a different `simulation` type altogether.
+
+/**
+ * @brief Used for event-based simulations.
+ */
+class YASF_EXPORT external_time_updater : public time_updater
+{
+public:
+    external_time_updater()
+        : time_updater("external_time_updater")
+    {
+    }
+
+    // clang-format-19 + gcc-14 doesn't like trailing return here.
+    // auto next_time() override -> time_usec;
+    // NOLINTBEGIN(modernize-use-trailing-return-type)
+
+    /**
+     * @brief
+     * @return
+     */
+    time_usec next_time() override { return m_next_time; }
+
+    // NOLINTEND(modernize-use-trailing-return-type)
+
+    auto set_next_time(time_usec next_time) -> void { m_next_time = next_time; }
+
+private:
+    time_usec m_next_time{};
+};
+
+}  // namespace yasf
