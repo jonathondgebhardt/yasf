f4c92eab9e808cb09ebe17a93ee4670c7fecabea
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2025-01-26 07:26:15 -0500

    Add spdlog as a dependency when installed

--- cmake/install-config.cmake
+++ cmake/install-config.cmake
@@ -2,6 +2,7 @@ set(yasf_FOUND YES)
 
 include(CMakeFindDependencyMacro)
 find_dependency(boost_uuid)
+find_dependency(spdlog)
 
 if(yasf_FOUND)
   include("${CMAKE_CURRENT_LIST_DIR}/yasfTargets.cmake")
