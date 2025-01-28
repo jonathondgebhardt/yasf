2a0c43b505952bbe199fac0ab6b6fa697cc2d74a
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2025-01-27 20:31:38 -0500

    Add more time types

--- include/yasf/types.hpp
+++ include/yasf/types.hpp
@@ -7,5 +7,8 @@ namespace yasf
 
 using time_usec = std::uint64_t;
 using time_sec = double;
+using time_minutes = double;
+using time_hours = double;
+using time_days = double;
 
 }  // namespace yasf
