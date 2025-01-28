f78331e27775f4bb8e15e425fbaae268f33e4047
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2025-01-27 21:19:21 -0500

    Add conversion functions for new time types

--- include/yasf/constants.hpp
+++ include/yasf/constants.hpp
@@ -6,4 +6,13 @@ namespace yasf::constants
 constexpr auto seconds_to_useconds = 1000000;
 constexpr auto useconds_to_seconds = 1.0 / seconds_to_useconds;
 
+constexpr auto minutes_to_seconds = 60.0;
+constexpr auto seconds_to_minutes = 1.0 / minutes_to_seconds;
+
+constexpr auto hours_to_minutes = minutes_to_seconds;
+constexpr auto minutes_to_hours = seconds_to_minutes;
+
+constexpr auto days_to_hours = 24.0;
+constexpr auto hours_to_days = 1.0 / days_to_hours;
+
 }  // namespace yasf::constants
--- include/yasf/convert.hpp
+++ include/yasf/convert.hpp
@@ -6,14 +6,44 @@
 namespace yasf::convert
 {
 
-constexpr auto seconds_to_useconds(time_seconds sec) -> time_useconds
+constexpr auto seconds_to_useconds(time_seconds seconds) -> time_useconds
 {
-    return static_cast<time_useconds>(sec * constants::seconds_to_useconds);
+    return static_cast<time_useconds>(seconds * constants::seconds_to_useconds);
 }
 
-constexpr auto useconds_to_seconds(time_useconds usec) -> time_seconds
+constexpr auto useconds_to_seconds(time_useconds useconds) -> time_seconds
 {
-    return static_cast<time_seconds>(usec) * constants::useconds_to_seconds;
+    return static_cast<time_seconds>(useconds) * constants::useconds_to_seconds;
+}
+
+constexpr auto minutes_to_seconds(time_minutes minutes) -> time_seconds
+{
+    return static_cast<time_seconds>(minutes * constants::minutes_to_seconds);
+}
+
+constexpr auto seconds_to_minutes(time_seconds seconds) -> time_minutes
+{
+    return static_cast<time_minutes>(seconds * constants::seconds_to_minutes);
+}
+
+constexpr auto hours_to_minutes(time_hours hours) -> time_minutes
+{
+    return static_cast<time_minutes>(hours * constants::hours_to_minutes);
+}
+
+constexpr auto minutes_to_hours(time_minutes minutes) -> time_hours
+{
+    return static_cast<time_hours>(minutes * constants::minutes_to_hours);
+}
+
+constexpr auto days_to_hours(time_days days) -> time_hours
+{
+    return static_cast<time_hours>(days * constants::days_to_hours);
+}
+
+constexpr auto hours_to_days(time_hours hours) -> time_days
+{
+    return static_cast<time_days>(hours * constants::hours_to_days);
 }
 
 }  // namespace yasf::convert
