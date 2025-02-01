4601a99f1dfb68afaf2e8d5e73ca7aeda575c812
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2025-02-01 17:59:45 -0500

    Add a test to verify processor_service updates child processors on update

--- test/source/processor_service_test.cpp
+++ test/source/processor_service_test.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <memory>
 
 #include "yasf/processor_service.hpp"
@@ -5,6 +6,7 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include "yasf/clock.hpp"
+#include "yasf/processor.hpp"
 #include "yasf/simulation.hpp"
 
 TEST_CASE("processor_service: name is processor_service", "[processor_service]")
@@ -31,3 +33,39 @@ TEST_CASE("processor_service: simulation getters", "[processor_service]")
         CHECK(psvc->get_clock() != nullptr);
     }
 }
+
+// NOLINTBEGIN(readability-function-cognitive-complexity)
+TEST_CASE("processor_service: update", "[processor_service]")
+{
+    struct test_processor : public yasf::processor
+    {
+        void update() override
+        {
+            m_visited = true;
+            ++m_visited_count;
+        }
+
+        bool m_visited{};
+        std::size_t m_visited_count{};
+    };
+
+    auto sim = yasf::simulation{std::make_unique<yasf::clock>()};
+
+    REQUIRE(sim.add_child<yasf::processor_service>());
+    auto* const psvc = sim.get_child<yasf::processor_service>();
+    REQUIRE(psvc != nullptr);
+
+    REQUIRE(psvc->add_child<test_processor>());
+    auto* const proc = psvc->get_child<test_processor>();
+    REQUIRE(proc != nullptr);
+
+    REQUIRE(!proc->m_visited);
+    REQUIRE(proc->m_visited_count == std::size_t{0});
+
+    REQUIRE_NOTHROW(proc->update());
+
+    CHECK(proc->m_visited);
+    CHECK(proc->m_visited_count == std::size_t{1});
+}
+
+// NOLINTEND(readability-function-cognitive-complexity)
