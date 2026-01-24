4d62eec36605ef9747f2421076923edadb9a19f8
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2026-01-24 18:13:51 -0500

    Add clock factory test.

--- test/source/clock_factory_test.cpp
+++ test/source/clock_factory_test.cpp
@@ -2,14 +2,24 @@
 
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("clock_factory: build fixed update", "[factory]")
+TEST_CASE("clock_factory: build fixed update", "[library][factory]")
 {
     auto obj = yasf::ClockFactory::build_fixed_update(yasf::time::Seconds{1.0});
     REQUIRE(obj != nullptr);
 
     SECTION("has fixed updater")
     {
-        auto* comp = obj->get_component("fixed_time_updater");
-        REQUIRE(comp != nullptr);
+        REQUIRE(obj->get_component("fixed_time_updater") != nullptr);
+    }
+}
+
+TEST_CASE("clock_factory: build external update", "[library][factory]")
+{
+    auto obj = yasf::ClockFactory::build_external_update();
+    REQUIRE(obj != nullptr);
+
+    SECTION("has external updater")
+    {
+        REQUIRE(obj->get_component("external_time_updater") != nullptr);
     }
 }
