af8b81a4e0ef939118fd335370244b719ea97199
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2026-01-25 20:36:49 -0500

    WIP behavior tree example.

--- examples/behavior-tree/source/main.cpp
+++ examples/behavior-tree/source/main.cpp
@@ -1,12 +1,12 @@
 #include <imgui-SFML.h>
 #include <imgui.h>
 
+#include "behavior_tree_drawable.hpp"
 #include "graphics_window.hpp"
-#include "node_drawable.hpp"
 #include "scene_manager.hpp"
+#include "yasf/behavior_tree.hpp"
 #include "yasf/clock_factory.hpp"
 #include "yasf/entity_service.hpp"
-#include "yasf/external_time_updater.hpp"
 #include "yasf/logger.hpp"
 #include "yasf/processor_service.hpp"
 #include "yasf/simulation.hpp"
@@ -21,21 +21,11 @@ auto make_sim() -> yasf::Simulation
     {
         auto svc = std::make_unique<yasf::EntityService>();
 
-        // ground
-        // svc->add_child(make_ground_object());
-
         sim.add_child(std::move(svc));
     }
 
     {
         auto svc = std::make_unique<yasf::ProcessorService>();
-
-        // auto mover = std::make_unique<EntityMover>();
-        // auto mover = std::make_unique<yasf::Mover>();
-        // svc->add_child(std::move(mover));
-        //
-        // auto ground_collision = std::make_unique<GroundCollisionProcessor>();
-        // svc->add_child(std::move(ground_collision));
 
         sim.add_child(std::move(svc));
     }
@@ -47,8 +37,6 @@ auto make_sim() -> yasf::Simulation
 
 auto main() -> int
 {
-    auto sim = make_sim();
-
     yasf::viewer::GraphicsWindow::init(
         {.window_width = 1024, .window_height = 768, .frame_limit = 60});
     auto* const window_handle = yasf::viewer::GraphicsWindow::instance();
@@ -58,28 +46,14 @@ auto main() -> int
         return EXIT_FAILURE;
     }
 
+    auto tree = yasf::BehaviorTree{};
+    tree.add_node(std::make_unique<yasf::DecoratorNode>());
+
     auto manager = yasf::viewer::SceneManager{};
-
-    const auto font = sf::Font{"/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"};
-    auto text_box = std::make_unique<NodeDrawable>(nullptr, font);
-    manager.add_drawable(std::move(text_box));
-
-    // GroundDrawable::build_drawables(sim, manager);
-
-    auto* const clock = sim.get_clock();
-    auto* const updater = clock->get_component<yasf::ExternalTimeUpdater>();
-
-    // auto sim_time_drawable = std::make_unique<SimTimeDrawable>();
-    // sim_time_drawable->clock = clock;
-    // manager.add_drawable(std::move(sim_time_drawable));
-    // manager.add_drawable(std::make_unique<MetricsDrawable>());
-    // auto sim_drawable = std::make_unique<TreeDrawable>();
-    // sim_drawable->sim = &sim;
-    // manager.add_drawable(std::move(sim_drawable));
+    auto behavior_tree_drawable = std::make_unique<BehaviorTreeDrawable>(&tree);
+    manager.add_drawable(std::move(behavior_tree_drawable));
 
     yasf::log::info("starting simulation visualization");
-
-    auto simulation_paused = false;
 
     // run the program as long as the window is open
     sf::Clock delta_clock;
@@ -106,21 +80,11 @@ auto main() -> int
 
         const auto delta_time = delta_clock.restart();
 
-        // todo: don't update simtime with SFML frame time
-        updater->set_next_time(
-            clock->time()
-            + yasf::time::Microseconds{delta_time.asMicroseconds()});
-
         ImGui::SFML::Update(*window_handle, delta_time);
 
         window_handle->clear();
 
         ImGui::Begin("Simulation");
-
-        ImGui::Checkbox("pause simulation", &simulation_paused);
-        if (!simulation_paused) {
-            sim.update();
-        }
 
         manager.draw();
         ImGui::End();
